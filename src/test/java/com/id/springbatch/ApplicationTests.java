0d788921bf811f2e6246777689b0e02dfc56d0cd
Abdessadiq <abdoubhm2018@gmail.com>
2023-08-14 18:37:55 +0100

    éLa creation de l'application..

---
+++ src/test/java/com/id/springbatch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
